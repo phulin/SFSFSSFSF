77adf58c015614953a83311d74268e8e9a5e6ffa
Patrick Hulin <patrick.hulin@gmail.com>
2011-11-27 17:55:37 -0500

    began work on flip_lowbits.c

--- src/flip_lowbits.c
+++ src/flip_lowbits.c
@@ -2,92 +2,54 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-
-#define BUF_SIZE 20480
-#define AUDIO_REFILL_THRESH 4096
+// number of 32-bit ints in a chunk
+#define CHUNK 4096
+#define COMMAND_LEN 8192
 
 #define err(msg) { fprintf(stderr, msg); exit(1); }
 
+// TODO: preserve metadata
+// TODO: parallelize?
+// TODO: insert minimum number of bits
+
 int main(int argc, char *argv[]) {
-	AVCodec *codec;
-	AVCodecContext *c = NULL;
-	AVFormatContext *fc = NULL;
-	uint8_t *outbuf;
-	uint8_t inbuf[BUF_SIZE + FF_INPUT_BUFFER_PADDING_SIZE];
-	AVPacket avpkt;
-	int len, out_size;
-	FILE *f;
+	FILE *fin, *pipein, *pipeout, *fout;
+	char command[COMMAND_LEN];
+	uint32_t buf[CHUNK];
 
 	if (argc < 1) err("Not enough arguments.\n");
 
-	// initialization
-	av_register_all();
-	avcodec_init();
+	fin = fopen(argv[1], "rb");
+	if (!fin) err("Couldn't open file for reading.\n");
 
-	// Open file
-	if (avformat_open_input(&fc, argv[1], NULL, NULL))
-		err("Couldn't open file.\n");
-
-	// Stream info
-	//if (avformat_find_stream_info(&fc, NULL) < 0)
-	//	err("Couldn't get stream info.\n");
+	snprintf(command, COMMAND_LEN, 
+	         "ffmpeg -i %s -f u32be pipe:", argv[1]);
 	
-	dump_format(fc, 0, argv[1], 0);
+	pipein = popen(command, "r");
+	if (!pipein) err("Couldn't open ffmpeg.\n");
 
-	// Assume it has two streams for now
-	c = fc->streams[0]->codec;
-	if (c == NULL) err("Couldn't get codec.\n");
+	// we'll write to a separate file to avoid screwing things up
+	snprintf(command, COMMAND_LEN,
+		     "ffmpeg -f u32be -ac 2 -ar 44100 -i pipe: -acodec alac .tmp.%s", argv[1]);
 
-	av_init_packet(&avpkt);
+	pipeout = popen(command, "w");
+	if (!pipeout) err("Couldn't open ffmpeg (2).\n");
 
-	// find codec
-	codec = avcodec_find_decoder(CODEC_ID_ALAC);
-	if (!codec) err("Couldn't find codec\n");
+	while (!feof(pipein)) {
+		int i;
+		size_t numread;
 
-	c = avcodec_alloc_context();
+		numread = fread(buf, 4, CHUNK, pipein);
+		if (numread < 0) err("Pipe error on read.\n");
 
-	// open codec
-	if (avcodec_open(c, codec) < 0) err("Couldn't open codec.\n");
-
-	av_read_frame(fc, &avpkt);
-
-	av_pkt_dump2(stderr, &avpkt, true, fc->streams[0]);
-
-	/*
-	outbuf = malloc(AVCODEC_MAX_AUDIO_FRAME_SIZE);
-
-	// ready to read
-	avpkt.data = inbuf;
-	avpkt.size = fread(inbuf, 1, BUF_SIZE, f);
-
-	while (avpkt.size > 0) {
-		out_size = AVCODEC_MAX_AUDIO_FRAME_SIZE;
-		len = avcodec_decode_audio3(c, (short *) outbuf, &out_size, &avpkt);
-
-		if (len < 0) err("Error while decoding.\n");
-
-		if (out_size > 0) {
-			int i;
-			for (i = 0; i < out_size; i++)
-			fwrite(outbuf, 1, out_size, stdout);
-		}
-
-		avpkt.size -= len;
-		avpkt.data += len;
-		if (avpkt.size < AUDIO_REFILL_THRESH) {
-			// refill input buffer
-			memmove(inbuf, avpkt.data, avpkt.size);
-			avpkt.data = inbuf;
-			len = fread(avpkt.data + avpkt.size, 1, BUF_SIZE - avpkt.size, stdin);
-			if (len > 0)
-				avpkt.size += len;
+		for (i = 0; i < numread; i++) {
+			buf[i] &= 0xFFFFFFFE;
 		}
 	}
-	*/
 
-	free(outbuf);
-	avcodec_close(c);
-	av_close_input_file(fc);
+	fclose(fin);
+	pclose(pipein);
+	pclose(pipeout);
+	fclose(fout);
+	return 0;
 }
